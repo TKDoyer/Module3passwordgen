062f5709a1b9028e119b25aad3a7834a5b9a63d5
Robert Doyer <tkdoyer@gmail.com>
2022-05-22 14:51:22 -0500

    write and rewrite

--- main.js
+++ main.js
@@ -1,8 +1,37 @@
+// DONT FORGET TO FINISH
+
+
+
 // Assignment code here
+const keys = {
+numbers:String [0,1,2,3,4,5,6,7,8,9,]
+symbol: "@$!*?^~% &",
+upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowerCase: "abcdefghijklmnopqrstuvwxyz"
 
-
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword(length, numbers, symbols, lowercase, uppercase) {
+    const availableCharacters = [
+        ...(numbers : []),
+        ...(symbols : []),
+        ...(lowercase : []),
+        ...(uppercase : []),
+    ];
+
+    let password = "";
+
+    if (availableCharacters.length === 0)
+        return "";
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * availableCharacters.length);
+        password += availableCharacters[randomIndex];
+    }
+    return password;
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -15,3 +44,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
